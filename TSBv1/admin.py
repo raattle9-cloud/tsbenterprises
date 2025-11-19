a5ddc36f47be4b6d6fdf052ed81a87a7a0876baa
Zimaad <zimaadazhari911@gmail.com>
2025-11-19 23:11:42 +0530

    fixed admin stuff

--- TSBv1/admin.py
+++ TSBv1/admin.py
@@ -5,20 +5,62 @@ from .models import Customer, Services, Cart, Payment, OrderPlaced
 
 @admin.register(Services)
 class ServicesModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'discounted_price', 'category', 'service_image']
+    list_display = ['id', 'title', 'discounted_price', 'selling_price', 'category', 'service_image']
+    list_filter = ['category']
+    search_fields = ['title', 'description']
+    list_editable = ['discounted_price', 'selling_price', 'category']
+    readonly_fields = ['id']
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'category', 'service_image')
+        }),
+        ('Pricing', {
+            'fields': ('selling_price', 'discounted_price')
+        }),
+        ('Details', {
+            'fields': ('description', 'composition', 'servapp')
+        }),
+    )
 
 @admin.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'name' ,'mobile', 'locality', 'city']
+    list_display = ['id', 'user', 'name', 'mobile', 'city', 'state', 'zipcode']
+    list_filter = ['state', 'city']
+    search_fields = ['name', 'mobile', 'user__username', 'user__email', 'locality', 'city']
+    readonly_fields = ['id']
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user', 'name')
+        }),
+        ('Contact Details', {
+            'fields': ('mobile',)
+        }),
+        ('Address', {
+            'fields': ('locality', 'city', 'state', 'zipcode')
+        }),
+    )
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-    list_display=['id', 'user', 'services', 'quantity']
+    list_display = ['id', 'user', 'services', 'quantity', 'total_cost']
+    list_filter = ['services__category']
+    search_fields = ['user__username', 'services__title']
+    readonly_fields = ['id', 'total_cost']
+    list_editable = ['quantity']
 
 @admin.register(Payment)
 class PaymentModelAdmin(admin.ModelAdmin):
-    list_display=['id', 'user', 'amount', 'razorpay_order_id', 'razorpay_payment_status', 'razorpay_payment_id', 'paid']
+    list_display = ['id', 'user', 'amount', 'paid', 'razorpay_order_id', 'razorpay_payment_status', 'razorpay_payment_id']
+    list_filter = ['paid', 'razorpay_payment_status']
+    search_fields = ['user__username', 'razorpay_order_id', 'razorpay_payment_id']
+    readonly_fields = ['id']
+    list_editable = ['paid']
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display=['id', 'user', 'customer', 'services', 'quantity', 'ordered_date', 'status','payment']
+    list_display = ['id', 'user', 'services', 'quantity', 'status', 'ordered_date', 'total_cost']
+    list_filter = ['status', 'ordered_date', 'services__category']
+    search_fields = ['user__username', 'services__title', 'customer__name']
+    readonly_fields = ['id', 'ordered_date', 'total_cost']
+    list_editable = ['status']
+    date_hierarchy = 'ordered_date'
